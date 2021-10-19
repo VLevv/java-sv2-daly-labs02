c6876f1b2e68ebf661d11f4c2e23d0f12252cb7d
VLevv <vadaslevi@gmail.com>
2021-10-19 15:39:12 +0200

    Konzultációs feladat

---
+++ week02-lab-tasks/src/main/java/day02/Geometry.java
@@ -0,0 +1,8 @@
+package day02;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(23.32,41.23);
+        System.out.println(rectangle.calculateArea());
+    }
+}
